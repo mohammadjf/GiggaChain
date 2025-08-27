4657f0ac02b8b57cdc57446b4afb8cdff06317ad
Mohammad Jafari <mohammadlngs@gmail.com>
2025-08-28 01:24:16 +0330

    fixed a bug: returning the result value instead of a response type

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -17,7 +17,7 @@ public class BaseApiController : Controller
 	protected ActionResult HandleResult<T>(Result<T> result)
 	{
 		if (!result.IsSuccess && result.Code == 404) return NotFound();
-		if (result.IsSuccess && result.Value != null) return Ok(result);
+		if (result.IsSuccess && result.Value != null) return Ok(result.Value);
 		return BadRequest(result.Error);
 	}
 }
